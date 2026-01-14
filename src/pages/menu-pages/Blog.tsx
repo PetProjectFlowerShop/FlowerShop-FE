52f1b06ab6ee0d407aab027d8b13ceba5c0d0f0c
IrynaIvash5 <110685624+IrynaIvash5@users.noreply.github.com>
2026-01-14 09:19:56 +0200

    feat(PPOFS-18): build API and config environment
    * feat(ppofs-18): build api axios
    
    * fix(PPOFS-28): added in .gitignore files; added .env.example
    
    * fix(PPOFS-18): resolved comments

--- src/pages/menu-pages/Blog.tsx
+++ src/pages/menu-pages/Blog.tsx
@@ -1,10 +1,31 @@
 import {PageContainer} from "../../components/common/PageContainer";
-import {Typography} from "@mui/material";
+import {Box, Typography} from "@mui/material";
+import {useEffect, useState} from "react";
+import {usersApi, User} from "../../api/users.api";
 
 export function Blog() {
+    const [users, setUsers] = useState<User[]>([])
+    useEffect(() => {
+        usersApi.getAll()
+            .then(res => {
+                setUsers(res.data)
+            })
+            .catch(error => {
+                console.error("Failed to fetch users:", error);
+            });
+    }, [])
     return (
         <PageContainer>
             <Typography variant="h4">Blog</Typography>
+
+            {/*just test data, can be deleted*/}
+            <Typography variant="h5">Users</Typography>
+            {users.map((user) => (
+                <Box key={user.id}>
+                    {user.name} - {user.email}
+                </Box>
+            ))}
+
         </PageContainer>
     );
 }
