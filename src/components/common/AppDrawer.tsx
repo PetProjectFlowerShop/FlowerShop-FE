3f1ea3c475ad5ff058d6adf943d9a87522a97b36
Ihor Larin <rmlarinig@gmail.com>
2026-01-18 13:13:50 +0200

    feat: implement AppDrawer with DrawerProvider and integrate LoginForm (#6)
    - Created `DrawerContext` and `DrawerProvider` to manage the drawer's open/closed state.
    - Added `useDrawer` hook for easy access to drawer state and toggle functionality.
    - Implemented `AppDrawer` component using Material UI `Drawer`.
    - Integrated `AppDrawer` into `MainLayout` and included `LoginForm` within it.
    - Updated `UserActions` to trigger the drawer toggle when the account icon is clicked.
    - Wrapped the application with `DrawerProvider` in `AppProviders`.

---
+++ src/components/common/AppDrawer.tsx
@@ -0,0 +1,25 @@
+import { Drawer, Box } from '@mui/material'
+import { useDrawer } from '../../hooks/useDrawer'
+import type { ReactNode } from 'react'
+
+type Props = {
+  children: ReactNode;
+}
+
+const AppDrawer = ({ children }: Props) => {
+  const { isDrawerOpen, toggleDrawer } = useDrawer()
+
+  return (
+    <Drawer
+      anchor="right"
+      open={isDrawerOpen}
+      onClose={toggleDrawer('auth', false)}
+    >
+      <Box display="flex" flexDirection="column" minWidth="40vw">
+        {children}
+      </Box>
+    </Drawer>
+  )
+}
+
+export default AppDrawer;
--- src/components/header/UserActions.tsx
+++ src/components/header/UserActions.tsx
@@ -1,7 +1,10 @@
-import {Box, IconButton} from "@mui/material";
-import {userActions} from "./data/actions.data";
+import { Box, IconButton } from '@mui/material'
+import { userActions } from './data/actions.data'
+import { useDrawer } from '../../hooks/useDrawer.ts'
 
 export function UserActions() {
+    const { toggleDrawer } = useDrawer()
+
     return (
         <Box sx={{ml: 2}}>
             {userActions.map((action) => (
@@ -9,6 +12,7 @@ export function UserActions() {
                     key={action.label}
                     aria-label={action.label}
                     sx={{color: 'text.primary'}}
+                    onClick={toggleDrawer(action.type, true)}
                 >
                     <Box
                         component="img"
@@ -23,5 +27,5 @@ export function UserActions() {
                 </IconButton>
             ))}
         </Box>
-    );
+    )
 }
