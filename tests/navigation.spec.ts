a18b24587540240be576075ba0a58233f4812ba6
Daria Vdovichenko <106913534+daria31v@users.noreply.github.com>
2026-02-16 21:52:40 +0100

    Feature/playwright (#12)
    * install playwright and added tests
    
    * Update README: added E2E setup
    
    * chore(ci): fix eslint
    
    * fix: regenerate package-lock.json after dependencies update
    
    ---------
    
    Co-authored-by: petelkavictor <p.v.victor322@gmail.com>

---
+++ tests/navigation.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('navigate to blog page', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByRole('link', { name: /blog/i }).first().click();
+
+  await expect(page.getByRole('heading', { name: /blog/i })).toBeVisible();
+});
