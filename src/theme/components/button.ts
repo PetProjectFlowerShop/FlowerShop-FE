c16de6afb65e1ee2e6d3350c0f317aa8b8eb4933
petelkavictor <p.v.victor322@gmail.com>
2026-02-19 14:28:06 +0200

    Styles/button (#23)
    * style: add custom button
    
    * style: add custom icon button
    
    * fix: resolve conversations

---
+++ src/theme/components/button.ts
@@ -0,0 +1,12 @@
+import type { Theme } from '@mui/material/styles';
+import type { Components } from '@mui/material/styles';
+
+export const buttonOverrides: Components<Theme>['MuiButton'] = {
+  styleOverrides: {
+    root: ({ theme }) => ({
+      padding: theme.spacing(2, 2.5),
+      borderRadius: '24px',
+      ...theme.typography.button,
+    }),
+  },
+};
