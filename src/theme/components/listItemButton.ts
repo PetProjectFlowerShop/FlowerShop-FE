aa823043852bba778963db42f008b2c0011da13a
petelkavictor <p.v.victor322@gmail.com>
2026-03-01 00:04:07 +0200

    Style/list pagination (#28)
    * chore: enable skipLibCheck in tsconfig
    
    * style: pagination
    
    * style: list
    
    * style: update palette
    
    * fix: resolve conversation

---
+++ src/theme/components/listItemButton.ts
@@ -0,0 +1,27 @@
+import type { Theme } from '@mui/material/styles';
+import type { Components } from '@mui/material/styles';
+
+export const listItemButtonOverrides: Components<Theme>['MuiListItemButton'] = {
+  styleOverrides: {
+    root: ({ theme }) => ({
+      '&.Mui-selected': {
+        backgroundColor: theme.palette.action.selected,
+      },
+      '&:hover': {
+        backgroundColor: theme.palette.action.hover,
+      },
+      '&.Mui-selected:hover': {
+        backgroundColor: theme.palette.action.selected,
+      },
+      '&.Mui-focusVisible': {
+        backgroundColor: theme.palette.action.focus,
+      },
+      '&:active': {
+        backgroundColor: theme.palette.action.active,
+      },
+    }),
+  },
+  defaultProps: {
+    disableRipple: true,
+  },
+};
