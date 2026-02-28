f082ec5daeef98f80cef6ba21c78b8ac88c66daa
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2026-02-28 20:07:34 +0100

    fix(api): include id in queryKey for user profile caching

--- src/api/user/useUserProfile.ts
+++ src/api/user/useUserProfile.ts
@@ -3,7 +3,7 @@ import { usersApi } from '../users.api';
 
 export const useUserProfile = (id: string) => {
   return useQuery({
-    queryKey: ['user', 'profile'],
+    queryKey: ['user', 'profile', 'id'],
     queryFn: () => usersApi.getById(id).then((res) => res.data),
   });
 };
