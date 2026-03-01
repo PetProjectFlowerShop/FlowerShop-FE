d7ca7e090c187ebb20ed78e550ee279a227f4843
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2026-03-01 16:10:10 +0100

    feat(data): update userActions to use IconName and sprite IDs

--- src/components/common/Icon.tsx
+++ src/components/common/Icon.tsx
@@ -1,5 +1,51 @@
+export type IconName =
+  | 'cancel'
+  | 'arrow'
+  | 'active-no'
+  | 'active-yes'
+  | 'add'
+  | 'apple'
+  | 'arrow-drop-down'
+  | 'arrow-drop-up'
+  | 'arrow-left'
+  | 'arrow-right'
+  | 'calendar'
+  | 'card'
+  | 'chevron-down'
+  | 'chevron-up'
+  | 'facebook'
+  | 'file-upload'
+  | 'google'
+  | 'instagram'
+  | 'minus'
+  | 'money'
+  | 'navigate-before'
+  | 'navigate-next'
+  | 'search'
+  | 'star-half'
+  | 'star-outline'
+  | 'star'
+  | 'telegram'
+  | 'checkbox'
+  | 'checkbox-outline'
+  | 'radiobutton-outlined'
+  | 'radiobutton'
+  | 'person'
+  | 'shopping-cart'
+  | 'time-watch'
+  | 'menu'
+  | 'mail'
+  | 'call'
+  | 'location'
+  | 'edit'
+  | 'close'
+  | 'visibility'
+  | 'visibility-off'
+  | 'vector'
+  | 'yes';
+
 type IconProps = {
-  name: string;
+  name: IconName;
 } & React.SVGProps<SVGSVGElement>;
 
 export function Icon({ name, ...props }: IconProps) {
