2c2813f4978b9e53273519b7b522f6fae1a3cc97
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2026-03-07 17:17:36 +0100

    feat(ui): implement icon sprite and theme overrides for core components

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,5 +1,11 @@
 import { buttonOverrides } from './button';
+import { dividerOverrides } from './divider';
+import { paperOverrides } from './paper';
+import { badgeOverrides } from './bage';
 
 export const components = {
   MuiButton: buttonOverrides,
+  MuiDivider: dividerOverrides,
+  MuiPaper: paperOverrides,
+  MuiBadge: badgeOverrides,
 };
