a6881baec01e57d7be5cef20842ad27252deea13
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2026-04-29 23:15:42 +0200

    fix(ui): breadcrumbs into ProductPage

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,14 +1,19 @@
 import { useParams } from 'react-router-dom';
 import CustomersReviewsSection from '../components/home/CustomersReviewsSection';
 import RecentlyViewedSection from '../components/home/RecentlyViewedSection';
+import { DynamicBreadcrumbs } from '../components/common/DynamicBreadcrumbs';
+import { PageContainer } from '../components/common/PageContainer';
 
 export function ProductPage() {
   const { id } = useParams();
+
   if (!id) return <div>No product</div>;
+
   return (
-    <>
+    <PageContainer>
+      <DynamicBreadcrumbs />
       <RecentlyViewedSection />
       <CustomersReviewsSection isReview={true} />
-    </>
+    </PageContainer>
   );
 }
