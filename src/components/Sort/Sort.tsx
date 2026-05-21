dbc26f76ecbd5bf2abe3b95aee88b6fb1887a965
AzVika <26838569+AzVika@users.noreply.github.com>
2026-05-21 14:25:42 +0300

    style: update sort

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -3,6 +3,7 @@ import { MenuItem, Select, type SelectChangeEvent } from '@mui/material';
 import { useSearchParams } from 'react-router-dom';
 import type { SortOption } from '../../types/sort';
 import { SORT_OPTIONS } from '../../constants/sort';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 export function Sort() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -19,9 +20,30 @@ export function Sort() {
     setSearchParams(newParams);
   };
   return (
-    <Select value={sort} onChange={handleChange}>
+    <Select
+      value={sort}
+      onChange={handleChange}
+      IconComponent={KeyboardArrowDownIcon}
+      sx={{
+        fontSize: '16px',
+        lineHeight: 1.5,
+        borderRadius: '20px',
+        padding: '0px 20px',
+        '.MuiOutlinedInput-notchedOutline': {
+          borderColor: 'primary.main',
+          borderWidth: 2,
+        },
+        '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+          borderWidth: 2,
+        },
+        '.MuiSelect-icon': {
+          marginRight: 2,
+          fontSize: 28,
+        },
+      }}
+    >
       {SORT_OPTIONS.map((opt) => (
-        <MenuItem key={opt.value} value={opt.value}>
+        <MenuItem key={opt.value} value={opt.value} sx={{ padding: '4px 23px' }}>
           {opt.label}
         </MenuItem>
       ))}
