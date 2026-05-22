938566d827392b9336db37bce2e068fc1aa4ab3d
Azomova Vika <26838569+AzVika@users.noreply.github.com>
2026-05-22 12:20:55 +0300

    style: update sort (#51)
    * style: update sort
    
    * style: fix
    
    ---------
    
    Co-authored-by: petelkavictor <p.v.victor322@gmail.com>

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -3,6 +3,7 @@ import { MenuItem, Select, type SelectChangeEvent } from '@mui/material';
 import { useSearchParams } from 'react-router-dom';
 import type { SortOption } from '../../types/sort';
 import { SORT_OPTIONS } from '../../constants/sort';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 export function Sort() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -19,9 +20,41 @@ export function Sort() {
     setSearchParams(newParams);
   };
   return (
-    <Select value={sort} onChange={handleChange}>
+    <Select
+      value={sort}
+      onChange={handleChange}
+      IconComponent={KeyboardArrowDownIcon}
+      sx={{
+        lineHeight: 1.5,
+        borderRadius: 3,
+        padding: '0px 20px',
+        '& .MuiSelect-select': {
+          display: 'flex',
+          alignItems: 'center',
+          fontSize: '16px',
+          minHeight: '20px',
+          maxHeight: '56px',
+        },
+        '& .MuiSelect-select.MuiOutlinedInput-input': {
+          pr: 6,
+          pl: 0,
+          py: '18px',
+        },
+        '.MuiOutlinedInput-notchedOutline': {
+          borderColor: 'primary.main',
+          borderWidth: 2,
+        },
+        '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+          borderWidth: 2,
+        },
+        '.MuiSelect-icon': {
+          right: 20,
+          fontSize: 24,
+        },
+      }}
+    >
       {SORT_OPTIONS.map((opt) => (
-        <MenuItem key={opt.value} value={opt.value}>
+        <MenuItem key={opt.value} value={opt.value} sx={{ padding: '4px 23px' }}>
           {opt.label}
         </MenuItem>
       ))}
