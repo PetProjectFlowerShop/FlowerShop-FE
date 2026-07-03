99c614390d4addf4faa964096436fb9c64f90105
petelkavictor <p.v.victor322@gmail.com>
2026-07-03 20:53:25 +0300

    feat: add test

---
+++ tests/visual/home.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('Home page VRT', async ({ page }) => {
+  await page.goto('/');
+
+  await expect(
+    page.getByRole('heading', {
+      level: 1,
+      name: /Charming blooms/i,
+    })
+  ).toBeVisible();
+
+  await expect(page).toHaveScreenshot('home-page.png', { fullPage: true });
+});
