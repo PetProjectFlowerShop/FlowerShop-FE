d2c9c707322659fa2ea059a42b7e5cb57ba6a5d2
petelkavictor <p.v.victor322@gmail.com>
2026-07-04 07:50:56 +0300

    test: check test work

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -43,7 +43,7 @@ export default function HeroSection() {
                 maxWidth: { xs: 'none', tablet: '200px', desktop: 'none' },
               }}
             >
-              Charming blooms.
+              Charming blooms123.
               <br />
               Special moments.
             </Typography>
