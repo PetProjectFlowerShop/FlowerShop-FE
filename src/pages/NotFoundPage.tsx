ca29b3f8dc2939156072e0b0de5c8e2d87971805
Daria Vdovichenko <106913534+daria31v@users.noreply.github.com>
2026-08-10 18:04:45 +0200

    fix(ui): added imgages for error pages, create page 404, changes routes (#85)

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,17 +1,34 @@
 import { Box, Button, Typography } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import imgNotFound from '@/assets/images/404.webp';
 
 export function NotFoundPage() {
-  const navigate = useNavigate();
-
   return (
-    <Box textAlign="center" py={6}>
-      <Typography variant="h4" gutterBottom>
-        404 – Page not found
+    <Box textAlign="center" sx={{ my: { xs: 20, tablet: 30 } }}>
+      <Box
+        component="img"
+        src={imgNotFound}
+        alt="Not found page"
+        sx={{
+          width: { xs: '300px', tablet: '600px' },
+        }}
+      />
+      <Typography variant="h3">Oops! We can't find this page.</Typography>
+      <Typography variant="body1" sx={{ mt: '16px' }}>
+        The page you’re looking for has withered away.
       </Typography>
-
-      <Button variant="contained" onClick={() => navigate('/')}>
-        Go to home
+      <Button
+        variant="contained"
+        sx={{
+          width: { xs: '344px', tablet: '400px' },
+          height: { xs: '56px', tablet: '60px' },
+          mt: '40px',
+          backgroundColor: 'secondary.main',
+        }}
+        component={Link}
+        to="/"
+      >
+        Go to home page
       </Button>
     </Box>
   );
