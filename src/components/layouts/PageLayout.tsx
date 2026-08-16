f59c893a3b1fb1580219a096a407b0046d109cef
Daria Vdovichenko <106913534+daria31v@users.noreply.github.com>
2026-08-16 23:08:10 +0200

    Feature/503 (#87)
    * fix(ui): created ServiceUnavailablePage and implementation in ErrorBoundary
    
    * fix(ui): added function ServiceUnavailablePage into ProductPage
    
    * fix: simulated error
    
    * fix(ui): crected ServiceUnavailablePage
    
    * fix: props fix layout-components
    
    * style: fix  style 503 page
    
    * fix: remove error
    
    * docs: add contributing guidelines
    
    * fix: remove test temporary
    
    * fix: remove test temporary
    
    ---------
    
    Co-authored-by: petelkavictor <p.v.victor322@gmail.com>

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -1,11 +1,13 @@
-import { Stack } from '@mui/material';
-import type { ReactNode } from 'react';
+import { Stack, type StackProps } from '@mui/material';
 
-export function PageLayout({ children }: { children: ReactNode }) {
+type PageLayoutProps = StackProps;
+
+export function PageLayout({ children, ...props }: PageLayoutProps) {
   return (
     <Stack
       spacing={{ xs: 16, tablet: 20, desktop: 30 }}
       marginBottom={{ xs: 16, tablet: 20, desktop: 30 }}
+      {...props}
     >
       {children}
     </Stack>
---
+++ src/pages/error/ServiceUnavailablePage.tsx
@@ -0,0 +1,54 @@
+import imgError from '@/assets/images/503.webp';
+import { PageLayout } from '@/components/layouts/PageLayout';
+import { SectionContainer } from '@/components/layouts/SectionContainer';
+import { Box, Button, Typography } from '@mui/material';
+
+export default function ServiceUnavailablePage() {
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
+  return (
+    <PageLayout mt={{ xs: 16, tablet: 30 }} mb={{ tablet: 30 }}>
+      <section>
+        <SectionContainer
+          sx={{
+            alignItems: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 10,
+          }}
+        >
+          <Box
+            component="img"
+            src={imgError}
+            alt=""
+            sx={{
+              maxWidth: '600px',
+              width: '100%',
+            }}
+          />
+          <Box textAlign={{ xs: 'left', tablet: 'center' }}>
+            <Typography variant="h3">Too many bouquets, too little time!</Typography>
+            <Typography variant="body1" sx={{ mt: '16px' }}>
+              Our florists are working at full speed to handle an influx of orders.
+              <Box component="br" sx={{ display: { xs: 'none', tablet: 'block' } }} />
+              Please take a breath and try refreshing the page in a few minutes.
+            </Typography>
+          </Box>
+          <Button
+            variant="contained"
+            fullWidth
+            sx={{
+              maxWidth: '400px',
+            }}
+            color="secondary"
+            onClick={handleRefresh}
+          >
+            Refresh Page
+          </Button>
+        </SectionContainer>
+      </section>
+    </PageLayout>
+  );
+}
