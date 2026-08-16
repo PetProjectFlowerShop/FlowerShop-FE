3b653f89b82d5076295048c2aa28be67736a386c
petelkavictor <p.v.victor322@gmail.com>
2026-08-16 23:33:09 +0300

    style: fix  style 503 page

--- src/pages/error/ServiceUnavailablePage.tsx
+++ src/pages/error/ServiceUnavailablePage.tsx
@@ -1,6 +1,7 @@
-import { Box, Button, Typography } from '@mui/material';
 import imgError from '@/assets/images/503.webp';
-import { PageContainer } from '@/components/common/PageContainer';
+import { PageLayout } from '@/components/layouts/PageLayout';
+import { SectionContainer } from '@/components/layouts/SectionContainer';
+import { Box, Button, Typography } from '@mui/material';
 
 export default function ServiceUnavailablePage() {
   const handleRefresh = () => {
@@ -8,41 +9,46 @@ export default function ServiceUnavailablePage() {
   };
 
   return (
-    <PageContainer>
-      <Box textAlign={'center'}>
-        <Box
-          component="img"
-          src={imgError}
-          alt=""
+    <PageLayout mt={{ xs: 16, tablet: 30 }} mb={{ tablet: 30 }}>
+      <section>
+        <SectionContainer
           sx={{
-            width: { xs: '300px', tablet: '600px' },
+            alignItems: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 10,
           }}
-        />
-        <Typography variant="h3" textAlign={{ xs: 'left', tablet: 'center' }}>
-          Too many bouquets, too little time!
-        </Typography>
-        <Typography
-          variant="body1"
-          sx={{ mt: '16px' }}
-          textAlign={{ xs: 'left', tablet: 'center' }}
         >
-          Our florists are working at full speed to handle an influx of orders.
-          <Box component="br" sx={{ display: { xs: 'none', tablet: 'block' } }} />
-          Please take a breath and try refreshing the page in a few minutes.
-        </Typography>
-        <Button
-          variant="contained"
-          sx={{
-            width: { xs: '344px', tablet: '400px' },
-            height: { xs: '56px', tablet: '60px' },
-            mt: '40px',
-            backgroundColor: 'secondary.main',
-          }}
-          onClick={handleRefresh}
-        >
-          Refresh Page
-        </Button>
-      </Box>
-    </PageContainer>
+          <Box
+            component="img"
+            src={imgError}
+            alt=""
+            sx={{
+              maxWidth: '600px',
+              width: '100%',
+            }}
+          />
+          <Box textAlign={{ xs: 'left', tablet: 'center' }}>
+            <Typography variant="h3">Too many bouquets, too little time!</Typography>
+            <Typography variant="body1" sx={{ mt: '16px' }}>
+              Our florists are working at full speed to handle an influx of orders.
+              <Box component="br" sx={{ display: { xs: 'none', tablet: 'block' } }} />
+              Please take a breath and try refreshing the page in a few minutes.
+            </Typography>
+          </Box>
+          <Button
+            variant="contained"
+            fullWidth
+            sx={{
+              maxWidth: '400px',
+            }}
+            color="secondary"
+            onClick={handleRefresh}
+          >
+            Refresh Page
+          </Button>
+        </SectionContainer>
+      </section>
+    </PageLayout>
   );
 }
