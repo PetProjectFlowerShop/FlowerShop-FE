ba3630c710f6597bf2f31a5bb36266b66f19e891
petelkavictor <p.v.victor322@gmail.com>
2026-08-25 13:29:44 +0300

    style: fix section header

--- src/components/common/SectionHeader.tsx
+++ src/components/common/SectionHeader.tsx
@@ -15,11 +15,7 @@ interface SectionHeaderProps {
 export const SectionHeader = ({ title, subtitle, extratitle, mb, gap }: SectionHeaderProps) => {
   return (
     <Box sx={{ mb: mb ?? { xs: 2, sm: 4 }, gap: gap ?? '16px', display: 'grid' }}>
-      {title && (
-        <Typography variant="h2" sx={{ mb: subtitle ? 1 : 0 }}>
-          {title}
-        </Typography>
-      )}
+      {title && <Typography variant="h2">{title}</Typography>}
       {subtitle && (
         <Typography variant="h4" color="text.secondary">
           {subtitle}
