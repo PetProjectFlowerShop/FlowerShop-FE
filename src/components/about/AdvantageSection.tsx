cdb20dac9ecfec00d666ff1033e38e758b6a9b3b
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2026-08-30 15:14:39 +0200

    fix(ui): created AdvantageSection

---
+++ src/components/about/AdvantageSection.tsx
@@ -0,0 +1,12 @@
+import { AdvantageList } from '../home/AboutUsSection/AdvantageList';
+import { SectionContainer } from '../layouts/SectionContainer';
+
+export function AdvantageSection() {
+  return (
+    <section>
+      <SectionContainer>
+        <AdvantageList />
+      </SectionContainer>
+    </section>
+  );
+}
