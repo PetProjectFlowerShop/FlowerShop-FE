25b3fa892479a5754b7a7f67519afb0106fdf22d
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2026-08-30 15:15:24 +0200

    fix(ui): created OurValuesSection

---
+++ src/components/about/OurValuesSection.tsx
@@ -0,0 +1,78 @@
+import { SectionContainer } from '../layouts/SectionContainer';
+import { SectionHeader } from '../common/SectionHeader';
+import { Box, Button, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { ValuesList } from '../home/AboutUsSection/ValuesList';
+import values from './../../assets/images/about-us-values.webp';
+
+export const OurValuesSection = () => {
+  return (
+    <section>
+      <SectionContainer>
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: { xs: '1fr', desktop: '526px 1fr' },
+            gap: { xs: 4, desktop: 6 },
+            mb: { desktop: 10 },
+            alignItems: 'stretch',
+          }}
+        >
+          <Box
+            sx={{
+              order: { xs: 1, desktop: 2 },
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+            }}
+          >
+            <SectionHeader title="Our flower values" />
+
+            <Typography variant="body1" sx={{ mb: 4 }}>
+              At FLORIA, we believe flowers are more than a gift — they are a way to express
+              emotions, celebrate moments, and bring beauty into everyday life. Our philosophy is
+              built on simplicity, freshness, and thoughtful design, where every bouquet is created
+              with care and intention.
+            </Typography>
+            <Typography variant="body1" sx={{ mb: 4 }}>
+              Our values:
+            </Typography>
+            <ValuesList />
+
+            <Button
+              variant="contained"
+              sx={{ mt: { xs: 4, tablet: 6, desktop: 6 } }}
+              fullWidth
+              component={Link}
+              to="/catalog"
+            >
+              Go to catalog
+            </Button>
+          </Box>
+          <Box
+            sx={{
+              order: { xs: 2, desktop: 1 },
+              height: '100%',
+              overflow: 'hidden',
+              borderRadius: 4,
+            }}
+          >
+            <Box
+              component="img"
+              src={values}
+              alt="Our flower values"
+              sx={{
+                display: 'block',
+                width: { xs: '343px', tablet: '100%' },
+                height: { xs: 'auto', desktop: '100%' },
+                objectFit: 'cover',
+                objectPosition: 'top center',
+              }}
+            />
+          </Box>
+        </Box>
+      </SectionContainer>
+    </section>
+  );
+};
