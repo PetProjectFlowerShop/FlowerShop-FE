043a914fca3ec1e893dca4e3b23b5203a2966f78
petelkavictor <p.v.victor322@gmail.com>
2026-09-07 23:25:57 +0300

    refactor: remove placeholder variant

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -126,9 +126,4 @@ export const typography = (theme: Theme) => ({
     fontSize: '14px',
     lineHeight: 1.42,
   },
-  placeholder: {
-    fontFamily: '"Quicksand", sans-serif',
-    fontSize: '16px',
-    lineHeight: 1.5,
-  },
 });
